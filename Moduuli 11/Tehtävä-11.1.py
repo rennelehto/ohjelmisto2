2f5d3f746905aa95f980b7d9cf28de5d1f43e026
rennel <renne.lehto@metropolia.fi>
2025-03-26 14:08:40 +0200

    eka

---
+++ Moduuli 11/Tehtävä-11.1.py
@@ -0,0 +1,42 @@
+'''
+
+Toteuta seuraava luokkahierarkia Python-kielellä: Julkaisu voi olla kirja tai lehti. Jokaisella julkaisulla on nimi.
+Kirjalla on lisäksi kirjoittaja ja sivumäärä, kun taas lehdellä on päätoimittaja. Kirjoita luokkiin myös tarvittavat
+alustajat. Tee aliluokkiin metodi tulosta_tiedot, joka tulostaa kyseisen julkaisun kaikki tiedot. Luo pääohjelmassa
+julkaisut Aku Ankka (päätoimittaja Aki Hyyppä) ja Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua). Tulosta molempien
+julkaisujen kaikki tiedot toteuttamiesi metodien avulla.
+'''
+
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+    def tulosta_tiedot(self):
+        print(f'{self.nimi}')
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, päätoimittaja):
+        super().__init__(nimi)
+        self.päätoimittaja = päätoimittaja
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f'Päätoimittaja: {self.päätoimittaja}')
+
+class Kirja(Julkaisu):
+    def __init__(self, nimi, kirjailija, sivumäärä):
+        super().__init__(nimi)
+        self.kirjailija = kirjailija
+        self.sivumäärä = sivumäärä
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f'Kirjailija: {self.kirjailija}, {self.sivumäärä} sivua.')
+
+julkaisut=[]
+julkaisut.append(Lehti("Aku Ankka", "Aki Hyyppä"))
+julkaisut.append(Kirja("Hytti n:o 6", "Rosa Liksom", "200"))
+
+for j in julkaisut:
+    j.tulosta_tiedot()
+

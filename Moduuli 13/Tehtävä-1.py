55dee3089d1732f48772a402f255201adfeacf33
rennel <renne.lehto@metropolia.fi>
2025-04-20 16:36:05 +0300

    k

---
+++ Moduuli 13/Tehtävä-1.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import math
+
+app = Flask(__name__)
+@app.route('/alkuluku/<luku>')
+def alkuluku(luku):
+    #args = request.args
+    number = int(luku)
+    for kerroin in range(2, number, 1):
+        if number % kerroin == 0:
+            vastaus = 'false'
+            break
+    else:
+        vastaus = 'true'
+
+    tulos = {
+        "Number" : number,
+        "isPrime" : vastaus
+    }
+
+    return tulos
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
---
+++ Moduuli 13/Tehtävä-2.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import mysql.connector
+#http://127.0.0.1:3000/kentt%C3%A4/EFHK
+app = Flask(__name__)
+@app.route('/kenttä/<ICAO>')
+def koodi(ICAO):
+    sql = f"SELECT name, municipality FROM airport WHERE ident = '{ICAO}'"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            name = rivi[0]
+            municipality = rivi[1]
+
+        vastaus = {
+            "ICAO": ICAO,
+            "Name": name,
+            "Municipality": municipality
+        }
+
+        return vastaus
+
+yhteys = mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        database='flight_game',
+        user='python',
+        password='1232',
+        autocommit=True
+)
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
